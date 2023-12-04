f7ceb558546136b153e0793a396040495fae82f4
Shivam Maniya <shivammaniya@Shivams-MacBook-Pro-2.local>
2023-12-04 14:17:00 -0500

    all lazy components added

--- client/src/pages/nurses.js
+++ client/src/pages/nurses.js
@@ -1,15 +1,7 @@
 import React,{lazy} from 'react';
 import { GET_ALL_NURSES } from '../graphql/nurse';
 import { useQuery } from '@apollo/client';
-
-/*
-Allow the nurse to enter vital signs:
-body temperature, heart rate, blood
-pressure, or respiratory rate.
-*/
-
-
-const NursesTable = lazy(()=> import('../components/tables/NursesTable'));
+import NursesTable from ('../components/tables/NursesTable');
 
 const NursesPage = () => {
   const { data } = useQuery(GET_ALL_NURSES);
